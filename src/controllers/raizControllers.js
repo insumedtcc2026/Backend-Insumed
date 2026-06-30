21263f080e160ed7530e50965b062982ad8bccdf
Analivia835 <ana.2411100441008@aluno.eteacv.faetec.rj.gov.br>
2026-06-30 08:49:08 -0300

    atualizando a rota raiz

--- src/controllers/raizControllers.js
+++ src/controllers/raizControllers.js
@@ -9,15 +9,13 @@ export default {
                         <title>Servidor Backend - NodeJs</title>
                     </head>
                     <body>
-                        <h2>Olá - este é o servidor do 2 Info</h2>
-                        <p> ana liviaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                        <h2>Olá - este é o servidor do TCC INSUMED </h2>
+                        <p> </p>
                         <h3>Rotas:</h3>
                         <h3>/ - Rota raiz</h3>
-                        <h3>/fornecedorcod -  get / consulta de fornecedores ordenado por código</h3>
-                        <h3>/fornecedornome - get / consulta de fornecedores ordenado por nome</h3>
-                        <h3>/fornecedor - post / cadastro de fornecedores </h3>
-                        <h3>/fornecedor/:codfor - put / alteração de fornecedores </h3>
-                        <h3>/fornecedor/:codfor - delete / exclusão de fornecedores </h3>
+                        <h3>/pacientesall -  get / consulta de pacientes</h3>
+                        <h3>/paciente - post / cadastro de pacientes</h3>
+                    
                         <img src="https://img.icons8.com/?size=100&id=64097&format=png&color=000000" alt="">
                     </body>
                     </html>
