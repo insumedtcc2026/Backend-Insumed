444fc3c83e8b9a6f3ccc8586b3706ea255cf1e01
insumedtcc2026 <insumedtcc2026@gmail.com>
2026-07-02 08:43:51 -0300

    criação da documentação da API

--- src/controllers/raizControllers.js
+++ src/controllers/raizControllers.js
@@ -1,28 +1,19 @@
+import { error } from "console";
+import path, { dirname } from "path";
+
 export default {
-    async raiz(req, res){
-        const html = `
-                    <!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Servidor Backend - NodeJs</title>
-                    </head>
-                    <body>
-                        <h2>Olá - este é o servidor do TCC INSUMED </h2>
-                        <p> </p>
-                        <h3>Rotas:</h3>
-                        <h3>/ - Rota raiz</h3>
-                        <h3>/pacientesall -  get / consulta de pacientes</h3>
-                        <h3>/paciente - post / cadastro de pacientes</h3>
-                    
-                        <img src="https://img.icons8.com/?size=100&id=64097&format=png&color=000000" alt="">
-                    </body>
-                    </html>
-                    
-                `;
+    raiz(req,res) {
+        const filePath = path.join(import.meta.dirname, '..','..','documents', 'api_documentacao.html');
+        
+        return res.sendFile(filePath, (err) => {
+            if (err) {
+                console.error('Erro ao enviar arquivo' , err);
+                res.status(err.status || 500).send ({
+                    msg: ' Erro ao carregar a pagina de documentação',
+                    error: err.message
 
-        return res.send(html);
+                });
+            }
+        });
     }
-}
-
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-import routes from "./routes.js"; // 1. Adicionada a extensão .js
+import routes from "./routes.js";
+import path from "path" // 1. Adicionada a extensão .js
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(import.meta.dirname, '..', 'public')));
 app.use(express.json());
 app.use(routes);
 
