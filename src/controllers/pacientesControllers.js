8b63d162fbb9a2970bb3b9f6cf9ea58ddebcaedd
Analivia835 <ana2411100441009@aluno.eteacv.faetec.rj.gov.br>
2026-07-02 21:00:13 -0300

    conectando com o frontend

--- src/controllers/pacientesControllers.js
+++ src/controllers/pacientesControllers.js
@@ -15,33 +15,67 @@ export default  {
 
 
 //cria um novo paciente
-   async createpaciente(req, res) {
-    const { nome , email, telefone, cpf, data_nasc, sexo, endereco, raça, senha } = req.body;
+  async createpaciente(req, res) {
+  try {
+    
 
-//criptografa a senha do paciente
-try {
- const hashSenha = await bcrypt.hash(req.body.senha, 10);
+    console.log("BODY:", req.body);
 
-const dadoscreate = {
-    'pac_nome': nome,
-    'pac_email': email,
-    'pac_telefone': telefone, 
-    'pac_senha': hashSenha,
-    'pac_cpf': cpf,
-    'pac_data_nasc': data_nasc, 
-    'pac_sexo': sexo,
-    'pac_endereço': endereco, 
-    'pac_raça': raça
-};
-     
-     await knex('pacientes').insert(dadoscreate);
- 
-     return res.status(201).send({ message: 'Paciente cadastrado com sucesso!' });
-   }
-   catch (erro){
-    return res.status(500).send({ message: 'Erro ao cadastrar novo paciente',error: erro.message });
-   }
-  
-   },
+    const {
+      nome,
+      email,
+      telefone,
+      cpf,
+      data_nasc,
+      sexo,
+      endereco,
+      raca,
+      senha
+    } = req.body;
+    const sexoLimpo = sexo?.trim().toLowerCase();
+
+let sexoFormatado = null;
+if (sexo?.toLowerCase() === "masculino") sexoFormatado = "M";
+else if (sexo?.toLowerCase() === "feminino") sexoFormatado = "F";
+else if (sexo?.toLowerCase() === "outro") sexoFormatado = "O";
+
+    console.log("NOME:", nome);
+    console.log("EMAIL:", email);
+    console.log("SENHA:", senha);
+
+    if (!senha) {
+      throw new Error("Senha está undefined");
+    }
+
+    const hashSenha = await bcrypt.hash(senha, 10);
+
+    const dadoscreate = {
+      pac_nome: nome,
+      pac_email: email,
+      pac_telefone: telefone,
+      pac_senha: hashSenha,
+      pac_cpf: cpf,
+      pac_data_nasc: data_nasc || null,
+      pac_sexo: sexoFormatado || null,
+      pac_endereco: endereco,
+      pac_raca: raca || null
+    };
+
+    console.log("DADOS PARA INSERT:", dadoscreate);
+
+    
+    await knex('pacientes').insert(dadoscreate);
+
+    console.log("SEXO ORIGINAL:", sexo);
+console.log("SEXO FORMATADO:", sexoFormatado);
+
+    res.status(201).json({ msg: "OK" });
+
+  } catch (erro) {
+    console.error(" ERRO NO BACKEND:", erro);
+    res.status(500).json({ erro: erro.message });
+  }
+}
+   
 
 }
