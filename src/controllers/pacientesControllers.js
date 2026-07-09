12e2eab89c092a1e52f5c879706161715048f94f
Analivia835 <ana2411100441009@aluno.eteacv.faetec.rj.gov.br>
2026-07-09 13:57:54 -0300

    resolvendo

--- src/controllers/pacientesControllers.js
+++ src/controllers/pacientesControllers.js
@@ -4,15 +4,13 @@ import bcrypt from 'bcrypt'
 export default  {
 //busca todos os dados da tabela pacientes
   async pacientesall(req, res) {
-    console.log("DADOS PARA INSERT:", dadoscreate);
-
-try {
-  await knex("pacientes").insert(dadoscreate);
-  console.log("INSERT REALIZADO COM SUCESSO");
-} catch (e) {
-  console.error("ERRO NO INSERT:", e);
-  throw e;
-}
+    try {
+      const nome = await knex('pacientes');
+      console.log(nome);
+      return res.status(200).send(nome);
+    } catch (error) {
+      res.status(500).send({ message: 'Erro ao buscar pacientes', error: error.message });
+    }
   },
 
 
